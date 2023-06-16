5a5ace9e6774a112ddaec34a0b010ebf2e4b6210
gongool <73773814+gongool@users.noreply.github.com>
2023-06-16 00:27:39 +0000

    Intializing files and starting  with Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import Header from "./components/Header/Header"
+import "./App.css"
 
 const App = () => {
   return (
-    <div>Hello World!</div>
+    <>
+    <Header />
+    </>
   )
 }
 
