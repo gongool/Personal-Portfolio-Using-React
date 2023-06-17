0c143f3942459ae39a0669c7159f8b10c10f42a3
gongool <73773814+gongool@users.noreply.github.com>
2023-06-17 02:35:45 +0000

    Initializing  The About Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from "./components/Header/Header"
 import Home from './components/Home/Home'
+import About from "./components/About/About"
 
 import "./App.css"
 
@@ -10,6 +11,7 @@ const App = () => {
       <Header />
       <main className='main'>
         <Home />
+        <About />
       </main>
     </>
   )
