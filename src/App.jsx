37e67d624d12129a3c97261f03aeae46b212c420
gongool <73773814+gongool@users.noreply.github.com>
2023-06-17 21:02:21 +0000

    Update 5 files

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from "./components/Header/Header"
 import Home from './components/Home/Home'
 import About from "./components/About/About"
+import Skills from './components/Skills/Skills'
 
 import "./App.css"
 
@@ -12,6 +13,7 @@ const App = () => {
       <main className='main'>
         <Home />
         <About />
+        <Skills />
       </main>
     </>
   )
