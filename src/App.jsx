865152ef8bfb192120c8c9b1d6206162d5c01959
gongool <73773814+gongool@users.noreply.github.com>
2023-06-19 17:05:33 +0000

    Starting with Contact component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
+import "./App.css"
 import Header from "./components/Header/Header"
 import Home from './components/Home/Home'
 import About from "./components/About/About"
 import Skills from './components/Skills/Skills'
 import Services from './components/Services/Services'
 import Qualifications from "./components/Qualifications/Qualifications"
+import Testimonials from './components/Testimonials/Testimonials'
+import Contact from './components/Contact/Contact'
 
-import "./App.css"
 
 const App = () => {
   return (
@@ -18,6 +20,8 @@ const App = () => {
         <Skills />
         <Services />
         <Qualifications />
+        <Testimonials />
+        <Contact />
       </main>
     </>
   )
---
+++ src/components/Testimonials/Data.jsx
@@ -0,0 +1,27 @@
+import Image1 from "../../assets/testimonial1.png";
+import Image2 from "../../assets/testimonial2.png";
+import Image3 from "../../assets/testimonial3.png";
+
+export const Data = [
+
+    {
+        id: 1,
+        image: Image1,
+        title: "Mohammed Adel",
+        description: "A really impressive job, every aspect of the project was meticulously executed, resulting in outstanding outcomes",
+    },
+    {
+        id: 2,
+        image: Image2,
+        title: "Ahmed Saraj",
+        description: "I couldn't be happier with the service provided. The team was incredibly professional, responsive, and delivered beyond my expectations. I highly recommend their services.",
+    },
+    {
+        id: 3,
+        image: Image3,
+        title: "Musab Mohammed",
+        description: "Working with this team has been a game-changer for my business. Their expertise and attention to detail have helped us achieve remarkable results. I look forward to collaborating with them again in the future.",
+    }
+        
+];
+
